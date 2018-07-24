8384aed84b9758dfe991e4cfeed0160b40036d33
cleophas.mashiri <cleophas.mashiri@standardbank.co.za>
2018-07-24 09:29:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo-automated-acceptance-testing-bdd
+A recipe for Automated-Acceptance-Testing with Cucumber and Serenity with feature files
---
+++ src/main/java/com/example/todoautomatedacceptancetestingbdd/TodoAutomatedAcceptanceTestingBddApplication.java
@@ -0,0 +1,12 @@
+package com.example.todoautomatedacceptancetestingbdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAutomatedAcceptanceTestingBddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAutomatedAcceptanceTestingBddApplication.class, args);
+	}
+}
